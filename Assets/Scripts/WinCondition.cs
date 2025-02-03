cd49ae819abeef78a7522412ccb493bb997f8b1b
MentalOfCrow <145018989+MentalOfCrow@users.noreply.github.com>
2025-02-03 11:19:24 +0100

    Create WinCondition.cs
    V1 Script
    
    Co-Authored-By: AbdallahYnov <145696378+AbdallahYnov@users.noreply.github.com>
    Co-Authored-By: Djibril132 <188316933+Djibril132@users.noreply.github.com>

---
+++ Assets/Scripts/WinCondition.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinCondition : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
