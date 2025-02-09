aaeda55cae4635f7d90d0156e80dacfd2b4485b5
MentalOfCrow <145018989+MentalOfCrow@users.noreply.github.com>
2025-02-09 15:00:12 +0100

    Update WinCondition.cs

--- Assets/Scripts/WinCondition.cs
+++ Assets/Scripts/WinCondition.cs
@@ -5,7 +5,7 @@ public class WinCondition : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player2"))
         {
             LoadNextLevel();
         }
